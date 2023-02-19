fcd379bb997bc01c80e7cb9a9570aa0c99c85e08
pojok code <pojokcodeid@gmail.com>
2023-02-19 17:49:53 +0700

    update

---
+++ lua/custom/lsp_installer.lua
@@ -0,0 +1,9 @@
+-- ini hanya untuk lsp yg tidak support mason
+-- untuk referesi support language kunjungi link dibawah
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local m = {
+	lspinstall = {
+		"yamlls", -- tambahkan di bawah sini untuk lsp tidak support mason
+	},
+}
+return m
