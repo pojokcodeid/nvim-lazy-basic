d17810977a84f2e2e5330b7792e670486436f141
pojok code <pojokcodeid@gmail.com>
2023-02-19 19:18:56 +0700

    config support dap

---
+++ lua/plugin/dapui.lua
@@ -0,0 +1,9 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	event = "BufWinEnter",
+	dependencies = "mfussenegger/nvim-dap",
+	enabled = vim.fn.has("win32") == 0,
+	config = function()
+		require("user.dapui")
+	end,
+}
