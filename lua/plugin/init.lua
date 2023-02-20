c74de23d8d512b1f3387a51503a3239d67785961
pojok code <pojokcodeid@gmail.com>
2023-02-20 23:31:16 +0700

    update

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,5 +1,6 @@
 -- custom plugins disini
 return {
+  {"nvim-treesitter/nvim-treesitter"},
 	--- masukan plugin tambahan disini
 	{ "folke/trouble.nvim", enabled = false }, -- ini untuk disable plugin
 }
