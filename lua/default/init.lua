8224f4b603ab7c5f0f6db32aab56649d4e0d2f09
pojok code <pojokcodeid@gmail.com>
2023-02-20 23:12:21 +0700

    update

--- lua/default/init.lua
+++ lua/default/init.lua
@@ -1,4 +1,10 @@
 require("default.lazy")
+-- default config
+require("user.options")
+require("user.keymaps")
+require("user.autocommands")
+require("user.format_onsave")
+-- user config
 require("default.options")
 require("default.autocommands")
 require("default.keymaps")
--- lua/plugin/default.lua
+++
@@ -1,6 +0,0 @@
-require("user.options")
-require("user.keymaps")
-require("user.autocommands")
-require("user.format_onsave")
--- require("user.treesitter")
-return {}
