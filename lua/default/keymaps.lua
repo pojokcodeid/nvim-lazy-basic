375e721b1382921ca887c35b6b761f491ec2d1d5
pojok code <pojokcodeid@gmail.com>
2023-02-22 11:04:30 +0700

    key mapping custom

--- lua/default/keymaps.lua
+++ lua/default/keymaps.lua
@@ -1,1 +1,6 @@
 -- custom key maps disini
+local function map(mode, l, r, desc)
+	vim.keymap.set(mode, l, r, { desc = desc })
+end
+-- ini adalah contoh
+map("n", "]h", '<cmd>lua print("Testing")<cr>', "Testing Mapping")
