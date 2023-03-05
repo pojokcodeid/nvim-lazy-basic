1725fce88ca7d01aacb603450e0960cecb5bd7b5
pojok code <pojokcodeid@gmail.com>
2023-03-05 11:27:44 +0700

    update config support custom color scheme

--- lua/custom/ui.lua
+++ lua/custom/ui.lua
@@ -14,22 +14,60 @@ m.lualine_style = 0
 -- 1 = vim icon " "
 -- 2 = vim icon " "
 m.status_icon = 0
+-- start custom lualine style
+-- contoh style
+--   {
+--     { left = "│", right = "│" },
+--     { left = " ", right = " " },
+--   },
+--   {
+--     { left = " ", right = " " },
+--     { left = " ", right = "" },
+--   },
+--   {
+--     { left = " ", right = " " },
+--     { left = "", right = "" },
+--   },
+--   {
+--     { left = "", right = "" },
+--     { left = "", right = "" },
+--   },
+--   {
+--     { left = " ", right = " " },
+--     { left = "", right = "" },
+--   },
+-- }
+m.custom_lualine = false
+m.component_separators = { left = " ", right = " " }
+m.section_separators = { left = "", right = " " }
+-- end custom lualine style
+
 -- custom colorscheme
 -- colorscheme ready :
-
--- tokyonight
--- tokyonight-night
--- tokyonight-storm
--- tokyonight-day
--- tokyonight-moon
+-- tokyonight, tokyonight-night, tokyonight-storm, tokyonight-day, tokyonight-moon
 -- gruvbox-baby (default)
--- sonokai
--- material
--- onedark
+-- sonokai, sonokai_atlantis,
+-- sonokai_andromeda,sonokai_shusia,sonokai_maia,sonokai_espresso
+-- material, material_deepocean, material_palenight, material_lighter, material_darker
+-- onedark, onedark_darker, onedark_cool, onedark_deep,onedark_warm,
+-- onedark_warmer, onedark_light
 -- lunar
 -- nord
--- catppuccin
+-- catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 -- dracula
+-- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
 m.colorscheme = "gruvbox-baby"
-
+-- custom transparent mode
+-- only support
+--  gruvbox-baby,
+--  tokyonight,
+--  sonokai,
+--  material
+--  onedark
+--  catppuccin
+--  nord
+--  dracula
+--  nightfox
+-- 0 =off   1= on
+m.transparent_mode = 0
 return m
