3e156c9a5b091368ff61a9195eaa24394f536c20
Pojok Code <pojokcodeid@gmail.com>
2024-06-21 06:19:18 +0700

    enc: config for testingf and debuging

--- lua/custom/default.lua
+++ lua/custom/default.lua
@@ -15,9 +15,10 @@
 -- darcula-dark
 -- juliana
 pcode.colorscheme = "dracula"
-
 -- 0 =off   1= on
 pcode.transparent_mode = 0
+
+-- https://github.com/nvim-lualine/lualine.nvim
 -- rounded
 -- roundedall
 -- square
@@ -30,13 +31,12 @@ pcode.lualinetheme = "roundedall"
 -- 1 lualine lsp progress
 -- 2 fidget progress
 pcode.progress = 1
-
 -- 0 = on full text mode info,
 -- 1 = on initial mode + logo
 -- 2 = logo only
 -- 3 = initial only
 -- 4 = off
-pcode.show_mode = 3
+pcode.show_mode = 0
 
 -- 1 ( format jalan)  0 (fromat off)
 pcode.format_on_save = 1
@@ -61,11 +61,6 @@ pcode.lspghost_text = false
 pcode.mason_ensure_installed = { -- sebelumnya register_lsp
 	-- "yamlls",
 	-- "intelephense",
-	-- "marksman",
-	-- "csharp_ls",
-	-- "clangd",
-	-- "dartls",
-	-- "kotlin_language_server",
 	-- tambahkan di bawah sini setelah melakukan :masoninstall
 }
 pcode.unregister_lsp = {
@@ -87,9 +82,9 @@ pcode.dap_ensure_installed = {
 -- https://github.com/folke/which-key.nvim
 pcode.whichkey = {
 	-- contoh penambahan
-	["k"] = {
-		name = " Example",
-		k = { '<cmd>lua print("Testing")<cr>', "Example" },
+	["r"] = {
+		name = "  Run",
+		j = { "<cmd>Jaq float<CR>", "Run With Jaq" },
 	},
 }
 
@@ -100,28 +95,39 @@ pcode.coderunner = {
 	html = "live-server $dir/$fileName",
 }
 
+-- https://github.com/nvim-tree/nvim-tree.lua
 -- 0 = normal
 -- 1 = float
 pcode.nvimtree_isfloat = 0
 
+-- https://github.com/roobert/tailwindcss-colorizer-cmp.nvim
 pcode.tailwindcolorizer = false
+
 -- https://github.com/Exafunction/codeium.vim
 pcode.codeium = false
+
 -- https://github.com/Exafunction/codeium.nvim
 pcode.codeium_nvim = false
+
 -- https://github.com/kevinhwang91/nvim-ufo
 pcode.nvimufo = false
+
 -- https://github.com/echasnovski/mini.indentscope
 pcode.indentscope = false
+
 -- https://github.com/echasnovski/mini.animate
 pcode.minianimate = false
 
+-- https://github.com/hrsh7th/nvim-cmp
 pcode.disable_cmpdoc = false
+
 -- https://github.com/rachartier/tiny-devicons-auto-colors.nvim
 pcode.adaptive_color_icon = false
+
 -- https://github.com/lukas-reineke/virt-column.nvim
 pcode.columnline = false
 
+-- https://github.com/nvim-telescope/telescope.nvim
 ---@alias telescope_themes
 ---| "cursor"   # see `telescope.themes.get_cursor()`
 ---| "dropdown" # see `telescope.themes.get_dropdown()`
@@ -129,10 +135,31 @@ pcode.columnline = false
 ---| "center"   # retain the default telescope theme
 pcode.telescope_theme_find_file = "center"
 pcode.telescope_theme_live_grep = "ivy"
+
+-- https://github.com/ThePrimeagen/refactoring.nvim
+pcode.refactoring = false
+
 -- https://github.com/kristijanhusak/vim-dadbod-ui
 pcode.database = false
 
+-- https://github.com/rest-nvim/rest.nvim
+pcode.rest_client = false
+
+-- https://github.com/mfussenegger/nvim-dap
+pcode.nvim_dap = false -- not support for windows os (auto config mason-nvim-dap)
+pcode.nvim_dap_javascript = false
+pcode.nvim_dap_php = false
+pcode.nvim_dap_python = false
+pcode.nvim_dap_go = false
+
+-- https://github.com/olimorris/neotest-phpunit
+pcode.phpunit = false
+-- https://github.com/nvim-neotest/neotest-python
+-- https://docs.pytest.org/en/7.1.x/getting-started.html
+pcode.pytest = false
 -- https://github.com/nvim-neotest/neotest-jest
 pcode.jest = false
 pcode.jest_command = "npm test -- "
 pcode.jest_config = "jest.config.mjs"
+-- https://github.com/fredrikaverpil/neotest-golang
+pcode.gotest = false
