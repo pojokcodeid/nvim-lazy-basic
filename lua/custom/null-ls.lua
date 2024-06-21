638b8bb60079f197c2e191b27dcb61a007789ede
Pojok Code <pojokcodeid@gmail.com>
2024-06-21 14:46:17 +0700

    enc: update default config null ls

--- lua/custom/null-ls.lua
+++ lua/custom/null-ls.lua
@@ -1,9 +1,8 @@
 local null_ls = require("null-ls")
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 local m = {
 	sources = {
-		formatting.stylua, -- tambahkan di bawah sini
+		null_ls.builtins.formatting.stylua, -- tambahkan di bawah sini
+		-- null_ls.builtins.diagnostics.flake8, -- tambahkan di bawah sini
 	},
 }
 return m
